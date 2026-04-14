79c9ecce5f023d792dc6b9e47bf5c4d4bd62030f
Artyom Abakumov <artyom.abakumov@red-soft.ru>
2026-04-14 22:05:35 +0300

    Fix build after merge

--- src/jrd/Package.h
+++ src/jrd/Package.h
@@ -128,6 +128,7 @@ public:
 	}
 
 	void releaseLock(thread_db*) { }
+	void reloadAst(thread_db* tdbb, bool erase) { }
 
 	const QualifiedName& getName() const noexcept { return name; }
 	void setName(const QualifiedName& value) { name = value; }
