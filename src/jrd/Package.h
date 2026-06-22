ca9ccef980f45b60ef1c1213e4a838325df2a38d
Artyom Abakumov <artyom.abakumov@red-soft.ru>
2026-06-22 14:39:19 +0300

    Revert "Always use handmade stmt and request to make constant value"
    This reverts commit 80ab0e5d35f74fec2ec4cbe0caea4f5aae7860e6.

--- src/jrd/Package.h
+++ src/jrd/Package.h
@@ -88,7 +88,7 @@ public:
 
 	bid getBlobId(thread_db* tdbb);
 
-	dsc* makeValue(thread_db* tdbb);
+	dsc* makeValue(thread_db* tdbb, Request* request);
 
 private:
 	void executeCsbNode(thread_db* tdbb, CompilerScratch* csb);
