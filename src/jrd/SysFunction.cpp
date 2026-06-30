7641d759a9b26d73dc2d24a47734a1a3a00a0a8a
Alex <32911478+craftmaster1231@users.noreply.github.com>
2026-06-30 07:47:21 +0300

    Merge commit from fork
    * Fix modulo crash on MIN % -1
    
    * Remove unnecessary check for dividend
    
    ---------
    
    Co-authored-by: Alex Skatkov <alexandr.skatkov@red-soft.ru>

--- src/jrd/SysFunction.cpp
+++ src/jrd/SysFunction.cpp
@@ -5646,12 +5646,14 @@ dsc* evlMod(thread_db* tdbb, const SysFunction*, const NestValueArray& args,
 		return &impure->vlu_desc;
 	}
 
+	const SINT64 dividend = MOV_get_int64(tdbb, value1, 0);
 	const SINT64 divisor = MOV_get_int64(tdbb, value2, 0);
 
 	if (divisor == 0)
 		status_exception::raise(Arg::Gds(isc_arith_except) << Arg::Gds(isc_exception_integer_divide_by_zero));
 
-	const SINT64 result = MOV_get_int64(tdbb, value1, 0) % divisor;
+	// Condition covers MIN % -1 UB case
+	const SINT64 result = (divisor == -1) ? 0 : dividend % divisor;
 
 	switch (impure->vlu_desc.dsc_dtype)
 	{
